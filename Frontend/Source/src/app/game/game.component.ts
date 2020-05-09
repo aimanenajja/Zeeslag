7f47e83ae6657300158d520499404e8fbe87cc75
Aimane Najja <aimane.najja@cegeka.com>
2020-05-09 20:39:08 +0200

    giving up for now ...

--- Frontend/Source/src/app/game/game.component.ts
+++ Frontend/Source/src/app/game/game.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { GameInfo } from '../_models/game-info.model';
+import { GameService } from '../_services/game.service';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
 export class GameComponent implements OnInit {
+  gameInfo: GameInfo;
 
-  constructor() { }
+  constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
+    this.gameService.gameInfo$.subscribe((x) => (this.gameInfo = x));
   }
-
 }
